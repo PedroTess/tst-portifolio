3b0c6964319a96490d0356a14095a2800c7bd1a1
PedroTess <pedrotessniari@gmail.com>
2024-11-26 20:38:50 -0300

    Add files via upload

---
+++ js/reveal.js
@@ -0,0 +1,79 @@
+window.revelar = ScrollReveal ({reset:true})
+
+// SOBRE MIM 
+
+revelar.reveal ('.efeito-img-sm', {
+    duration: 2000, 
+    distance: '90px',
+    origin: 'left'
+})
+
+revelar.reveal ('.efeito-txt-sm', {
+    duration: 1500, 
+    distance: '90px',
+    origin: 'right'
+})
+
+revelar.reveal ('.titulo', {
+    duration: 1500, 
+    distance: '20px'
+})
+
+revelar.reveal ('.efeito-txt-hb', {
+    duration: 1500, 
+    distance: '60px'
+})
+
+revelar.reveal ('.img-hb1', {
+    duration: 1000, 
+    distance: '40px',
+    delay: 125
+})
+revelar.reveal ('.img-hb2', {
+    duration: 1000, 
+    distance: '40px',
+    delay: 250
+
+})
+revelar.reveal ('.img-hb3', {
+    duration: 1000, 
+    distance: '40px',
+    delay: 375
+})
+revelar.reveal ('.img-hb4', {
+    duration: 1000, 
+    distance: '40px',
+    delay: 500
+})
+revelar.reveal ('.img-hb5', {
+    duration: 1000, 
+    distance: '40px',
+    delay: 625
+})
+revelar.reveal ('.img-hb6', {
+    duration: 1000, 
+    distance: '40px',
+    delay: 750
+})
+revelar.reveal ('.img-hb7', {
+    duration: 1000, 
+    distance: '40px',
+    delay: 875
+})
+revelar.reveal ('.img-hb8', {
+    duration: 1000, 
+    distance: '40px',
+    delay: 1000
+})
+
+// PROJETOS
+revelar.reveal ('.pj-esq', {
+    duration: 1500, 
+    distance: '40px',
+    origin: 'left'
+})
+revelar.reveal ('.pj-drt', {
+    duration: 1500, 
+    distance: '40px',
+    origin: 'right'
+})
---
+++ js/scripts.js
@@ -0,0 +1,57 @@
+
+let navBar = document.querySelector('#header')
+
+document.addEventListener("scroll", ()=>{
+    let scrollTop = window.scrollY
+
+    if(scrollTop > 0){
+        navBar.classList.add('rolar')
+    }else{
+        navBar.classList.remove('rolar')
+    }
+})
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        const headerOffset = document.querySelector('header').offsetHeight;
+        const elementPosition = targetElement.getBoundingClientRect().top;
+        const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: "smooth"
+        });
+    });
+});
+
+// Menu de navegação mobile
+
+let btnMenuMob = document.querySelector ('#btn-menu-mob')
+let line1 = document.querySelector ('.line-menumob-1')
+let line2 = document.querySelector ('.line-menumob-2')
+let menuMobile = document.querySelector ('#menu-mobile') 
+let body = document.querySelector ('body')
+
+btnMenuMob.addEventListener("click",()=>{
+    line1.classList.toggle('ativo1')
+    line2.classList.toggle('ativo2')
+    menuMobile.classList.toggle('abrir')
+    body.classList.toggle('no-overflow')
+})
+
+// Fechar o menu mobile ao clicar em um link
+document.querySelectorAll('#menu-mobile a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', () => {
+        // Verifica se o menu está aberto e, se sim, fecha
+        if (menuMobile.classList.contains('abrir')) {
+            line1.classList.remove('ativo1');
+            line2.classList.remove('ativo2');
+            menuMobile.classList.remove('abrir');
+            body.classList.remove('no-overflow');
+        }
+    });
+});
